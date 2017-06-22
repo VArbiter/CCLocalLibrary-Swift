aec4799e53125af3c8579c69ee600fee0d0d7cfa
冯明庆 <elwinfrederick@163.com>
2017-06-22 18:09:17 +0800

    backup.

--- CCLocalLibrary-Swift/CCLocalLib-Swift/LocalLib/View/CCViewControllerExtension.swift
+++ CCLocalLibrary-Swift/CCLocalLib-Swift/LocalLib/View/CCViewControllerExtension.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2017 冯明庆. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func ccDismiss(alertController : UIAlertController? , after delay : Double) {
+        self.ccDismiss(alertController: alertController, after: delay, complete: nil);
+    }
+    func ccDismiss(alertController : UIAlertController? ,
+                   after delay : Double ,
+                   complete closureComplete : (() -> Void)? ) {
+        guard alertController != nil else {
+            return ;
+        }
+        
+        if delay > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                alertController?.dismiss(animated: true, completion: {
+                    if let closureCompleteT = closureComplete {
+                        closureCompleteT();
+                    }
+                })
+            });
+        }
+    }
+    
+    class func ccGetCurrentController() -> UIViewController? {
+        if let controllerT = self.ccGetCurrentViewController() {
+            return controllerT;
+        }
+        if let controllerRT = self.ccGetCurrentModalController() {
+            return controllerRT;
+        }
+        return nil;
+    }
+    private class func ccGetCurrentViewController() -> UIViewController? {
+        var window : UIWindow? = UIApplication.shared.keyWindow;
+        if window?.windowLevel != UIWindowLevelNormal {
+            for item in UIApplication.shared.windows {
+                if item.windowLevel == UIWindowLevelNormal {
+                    window = item;
+                    break;
+                }
+            }
+        }
+        let viewFront : UIView? = window?.subviews.first;
+        let controller : Any? = viewFront?.next;
+        
+        if "\(UIViewController.self)".compare("\(Mirror.init(reflecting: controller!).subjectType)") == .orderedSame {
+            return controller as? UIViewController;
+        }
+        return window?.rootViewController;
+    }
+    private class func ccGetCurrentModalController() -> UIViewController? {
+        
+    }
+    
+}
+
+
